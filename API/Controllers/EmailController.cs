8e033e927eb76ff0fc80f736a1559de48cb0b90e
danushkadi <danushkad@ccl.lk>
2025-06-19 15:43:31 +0530

    Added Email controller

---
+++ API/Controllers/EmailController.cs
@@ -0,0 +1,48 @@
+﻿using EmailService.Application.DTOs;
+using EmailService.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendEmail([FromForm] EmailRequestDto emailRequest, List<IFormFile>? files)
+        {
+            // Convert uploaded files to attachment DTOs
+            var attachments = new List<AttachmentDto>();
+
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    using var ms = new MemoryStream();
+                    await file.CopyToAsync(ms);
+
+                    attachments.Add(new AttachmentDto
+                    {
+                        FileName = file.FileName,
+                        FileData = ms.ToArray(),
+                        ContentType = file.ContentType
+                    });
+                }
+            }
+
+            var result = await _emailService.SendEmailAsync(emailRequest, attachments);
+
+            if (result)
+                return Ok(new { message = "Email sent successfully" });
+            else
+                return StatusCode(500, new { message = "Failed to send email" });
+        }
+    }
+}
